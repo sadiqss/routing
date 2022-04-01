984c5e8ed7a06784417279f93ee7fb51632a07bd
sadiqss <sadiq.sau48@gmail.com>
2022-04-01 14:21:11 +0100

    Dynamic path

--- src/compo/Friends/Friends.js
+++ src/compo/Friends/Friends.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setFriends(data));
+    }, [])
     return (
         <div>
             <h2>Hello friends</h2>
-            <p>Neeed money</p>
+            <p>Neeed money: {friends.length}</p>
+            {
+                friends.map(friend => <Friend key={friend.id}
+                    friend={friend}></Friend>)
+            }
         </div>
     );
 };
