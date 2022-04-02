a085eb296bd81dd13f874cd816d9284c5112e52e
sadiqss <sadiq.sau48@gmail.com>
2022-04-02 01:52:47 +0100

    nested routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Friends from './compo/Friends/Friends';
 import Header from './compo/Header/Header';
 import Home from './compo/Home/Home';
 import NotFound from './compo/NotFound/NotFound';
+import Post from './compo/Post/Post';
+import PostDetail from './compo/PostDetail/PostDetail';
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/friends' element={<Friends></Friends>}></Route>
         <Route path="/friends/:friendId" element={<FriendDetail></FriendDetail>}></Route>
+        <Route path="/posts" element={<Post></Post>}>
+          <Route path=':postId' element={<PostDetail></PostDetail>}></Route>
+        </Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
