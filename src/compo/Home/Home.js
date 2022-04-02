e9e0fe71fdda932dc541c0fd426499eb91459ba2
sadiqss <sadiq.sau48@gmail.com>
2022-04-02 02:40:15 +0100

    search

--- src/compo/Home/Home.js
+++ src/compo/Home/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Meal from '../Meal/Meal';
 
 const Home = () => {
+    const [searchText, setsearchText] = useState('');
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setMeals(data.meals));
+    }, [searchText])
+    const searchItem = e => {
+        setsearchText(e.target.value);
+    }
     return (
         <div>
             <h2>This is Home</h2>
             <p>Place for sleep</p>
+            <input onChange={searchItem} type="text" name='' id='' />
+            <br />
+            <h3>Result Found:{meals.length}</h3>
+            <div>
+                {
+                    meals.map(meal => <Meal
+                        key={meal.idMeal}
+                        meal={meal}
+                    ></Meal>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/compo/Meal/Meal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Meal.css'
+
+const Meal = (props) => {
+    const { strMeal, strInstructions, strMealThumb } = props.meal;
+    const { handleAddToOrder, meal } = props;
+    return (
+        <div className='meal'>
+            <img src={strMealThumb} alt='' />
+            <h4>{strMeal}</h4>
+            <p>{strInstructions.slice(0, 100)}</p>
+            <button onClick={() => handleAddToOrder(meal)}>Add this food</button>
+
+        </div>
+    );
+};
+
+export default Meal;
